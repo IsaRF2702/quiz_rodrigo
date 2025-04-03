04ec5d4580616ab38f68eeed1938a280204b04f8
IsaRF2702 <isabela.figueira@aluno.senai.br>
2025-04-03 13:41:00 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,3 +1,5 @@
+//ESSA AQUI É DEDICADA AOS USUÁRIOS
+
 import express from "express";
 import sql from "./database.js"
 import { CompararHash, CriarHash } from "./utilits.js";
@@ -6,7 +8,7 @@ const routes = express.Router()
 
 routes.post('/login', async (req, res) => {
     const { email, senha } = req.body
-
+try{ 
     let usuario = await sql`select 
     * from login where email=${email}
      and senha=${senha}`
@@ -19,20 +21,39 @@ routes.post('/login', async (req, res) => {
     else {
         return res.status(401).json('Usuario ou senha incorreto!')
     }
-
-    if (usuario.length != 0)
-        return res.status(200).json('cadastrado com sucesso')
-    else
-        res.status(400).json('Usuário não encontrado')
+}
+   
+    catch(error){
+        return res.status(500).json('Erro de servidor')
+    }
 })
 
 routes.post('/cadastrar', async (req, res) => {
+    try{
     const { email, senha } = req.body
-
     const hash = await CriarHash(senha, 10)
 
     await sql`INSERT INTO login(email, senha, funcao) values (${email}, ${hash},'aluno')`
     return res.status(200).json('cadastrado com sucesso')
+
+}
+    catch(error){
+        return res.status(500).json('Erro de Servidor')
+    }
 })
+
+// ESSA É DEDICADA ÁS QUESTÕES
+
+routes.post('/quest/cadastrar', async (req, res)=>{
+    try{
+        const {question, option1, option2, option3, option4, gabarito, nivel} = req.body;
+        const insert = await sql `insert into quest(question, option1, option2, option3, option4, gabarito, nivel)
+        values (${question},${option1},${option2},${option3},${option4}, ${gabarito}, ${nivel})` 
+        return res.status(200).json('ok')
+        }
+    catch(error){
+        return res.status(500).json('Erro de Servidor')
+    }
+});
 
 export default routes
