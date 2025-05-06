5ddf294a133b48a2d088f262ee9ceeefdeeb5530
IsaRF2702 <isabela.figueira@aluno.senai.br>
2025-05-06 08:26:35 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,3 +1,3 @@
 import postgres from 'postgres';
-const sql = postgres('postgres://root:root@192.168.1.15:5432/database');
+const sql = postgres('postgres://root:root@192.168.1.15:5432/Kauan_H');
 export default sql;
